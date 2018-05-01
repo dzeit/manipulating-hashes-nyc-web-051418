2eb345b2e20b76104e812404cea9db74f89f898a
David Zeit <dzeit216@gmail.com>
2018-05-01 18:03:45 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,6 +8,9 @@ def second_challenge
 
   #code your solution here!
 
-  
+groceries.values.flatten
+
+#"one dimensional" or "flat" array is a regular array or collection. Multi-dimensional is an array within an array or a collection within a collection.
+
 
 end
